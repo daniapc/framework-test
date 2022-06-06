cd8f18aa44a70e6f4ee07dde9595f29f7fbf4ccb
daniapc <daniel.castro0811@gmail.com>
2022-06-05 23:08:34 -0300

    Creating a Navegation bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  
+  #get 'home/index'
+  get 'home/aboutus'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
